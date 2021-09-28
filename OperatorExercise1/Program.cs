78699470b7c98d84a9adc22363919989978c5b83
nehamelville <neha.melville@gmail.com>
2021-09-28 08:56:44 -0700

    initial commit

---
+++ OperatorExercise1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+namespace OperatorExercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            int quotient, remainder;
+
+
+            Console.Write("Enter value for a: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter value for b: ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            quotient = Convert.ToInt32(a / b);
+            remainder = Convert.ToInt32(a % b);
+
+            Console.WriteLine(a + "/" + b + " is " + quotient +" and remainder is "+ remainder);
+
+
+        }
+    }
+}

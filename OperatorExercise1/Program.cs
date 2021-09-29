a381cb679607f6e9b5f0b4427b823da4d3fb062d
nehamelville <neha.melville@gmail.com>
2021-09-29 14:57:37 -0700

    code modified with AreaOfCircle() method

--- OperatorExercise1/Program.cs
+++ OperatorExercise1/Program.cs
@@ -11,7 +11,6 @@ namespace OperatorExercise1
             int b;
             int quotient, remainder;
 
-
             Console.Write("Enter value for a: ");
             a = Convert.ToInt32(Console.ReadLine());
             Console.Write("Enter value for b: ");
@@ -20,9 +19,31 @@ namespace OperatorExercise1
             quotient = Convert.ToInt32(a / b);
             remainder = Convert.ToInt32(a % b);
 
-            Console.WriteLine(a + "/" + b + " is " + quotient +" and remainder is "+ remainder);
+            Console.WriteLine(a + "/" + b + " is " + quotient + " and remainder is " + remainder);
+            AddSpaces(2);
+
+            Console.Write("What is the radius of your circle? ");
+            var radius = double.Parse(Console.ReadLine());
+            double area = AreaOfCircle(radius);
+            Console.WriteLine($"The area of circle with radius of {radius} is {area} ");
 
 
+        }
+        public static double AreaOfCircle(double r)
+        {
+            const double pi = 3.14f;
+            double area = pi * r * r;
+            return area;
+
+        }
+        public static void AddSpaces(int x)
+        {
+            while(x!=0)
+            {
+                Console.WriteLine("");
+                x--;
+            }
+            
         }
     }
 }
